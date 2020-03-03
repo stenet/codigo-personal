4fdb53f9e44cdebf11fc8ff979a9b76f4b9dd53d
Stefan Heim <stefan.heim@tip.co.at>
2020-03-03 09:03:04 +0100

    update nav scrolling

--- src/elements/nav/codigo-nav.ts
+++ src/elements/nav/codigo-nav.ts
@@ -2,5 +2,25 @@ import { inject } from "aurelia";
 
 @inject()
 export class CodigoNav {
+  onNavItemClick(ev: any) {
+    ev.preventDefault();
+    ev.stopPropagation();
 
+    const target: HTMLAnchorElement = ev.target;
+    const hash = target.hash;
+
+    if (!hash) {
+      return;
+    }
+
+    const el = document.querySelector(hash);
+    if (!el) {
+      return;
+    }
+
+    const offset = -50;
+    const y = el.getBoundingClientRect().top + window.pageYOffset + offset;
+
+    window.scrollTo({top: y, behavior: "smooth"});
+  }
 }
